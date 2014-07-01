78c8db012043b9362588e987f8fef8322eb9fd6f
Derek Tor <derekmuoktor@gmail.com>
2014-07-01 13:59:30 -0700

    boundary movement working

---
+++ RobotWar.spritebuilder/Source/Robot/CarpoolBot.h
@@ -0,0 +1,13 @@
+//
+//  CarpoolBot.h
+//  RobotWar
+//
+//  Created by Derek Tor on 7/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Robot.h"
+
+@interface CarpoolBot : Robot
+
+@end
---
+++ RobotWar.spritebuilder/Source/Robot/CarpoolBot.m
@@ -0,0 +1,81 @@
+//
+//  CarpoolBot.m
+//  RobotWar
+//
+//  Created by Derek Tor on 7/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CarpoolBot.h"
+
+@implementation CarpoolBot
+
+- (void)run {
+    [self firstTurn];
+    while (true) {
+        [self moveAhead:10];
+        [self turnInsideBoundaries];
+    }
+}
+
+-(void)firstTurn{
+    CGPoint origin = CGPointMake(0, 0);
+    [self getDistanceFromCGPoint:origin];
+    //if current timestamp == 0 then turn
+    if (self.currentTimestamp == 0) {
+        [self turnRobotRight:45];
+        [self turnGunLeft:45];
+        [self turnRobotRight:45];
+        [self moveAhead:100];
+        [self shoot];
+        [self turnGunLeft:45];
+    }
+}
+
+-(void)wallPresent{
+    
+}
+//if robot is less than 100 from boundary, turn left 90 and move forward 50
+
+-(void)turnInsideBoundaries{
+    
+    float maxWidth=[self arenaDimensions].width,
+    maxHeight=[self arenaDimensions].height,
+    x=self.robotBoundingBox.origin.x,
+    y=self.robotBoundingBox.origin.y;
+    
+    CGPoint bottom = CGPointMake(x, 0),
+    top = CGPointMake(x, maxHeight),
+    left = CGPointMake(0, y),
+    right = CGPointMake(maxWidth, y);
+    //(x,0),(x,max)
+    //(0,y),(max,y)
+    
+    if (![self  checkDistanceFromTop:top Bottom:bottom Left:left AndRight:right Within:50]) {
+        [self turnRobotLeft:45];
+        [self moveAhead:25];
+    }
+}
+
+-(BOOL)checkDistanceFromTop:(CGPoint)top Bottom:(CGPoint)bottom Left:(CGPoint)left AndRight:(CGPoint)right Within:(float)maxArea{
+    if ([self getDistanceFromCGPoint:top]>maxArea &&
+        [self getDistanceFromCGPoint:bottom]>maxArea &&
+        [self getDistanceFromCGPoint:left]>maxArea &&
+        [self getDistanceFromCGPoint:right]>maxArea) {
+        return true;
+    }
+    return false;
+}
+
+-(float)getDistanceFromCGPoint:(CGPoint)position{
+    float
+    firstX=self.robotBoundingBox.origin.x,
+    firstY=self.robotBoundingBox.origin.y,
+    secondX=position.x,
+    secondY=position.y,
+    distance=sqrtf((firstX-secondX)*(firstX-secondX)+(firstY-secondY)*(firstY-secondY));
+    NSLog(@"Distance is %f",distance);
+    return distance;
+}
+
+@end
